f868589c86a8e9f73c72ce9f397d5822adeceabf
Miriam <61996077+glitsch@users.noreply.github.com>
2022-05-19 17:02:06 +0200

    Update views.py

--- part_2/todo/views.py
+++ part_2/todo/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from .models import Item
 
 # Create your views here.
-
-def home(response):
-    list = {}
-    return render(response, "todo/base.html", {})
-
 def todolist(request):
     data = Item.objects.all()
-    return render(request, "todo/home.html",{'data':data})
+    return render(request, "todo/home.html", {'data':data})
+
+def impressum(request):
+    return render(request, "todo/impressum.html", {})
