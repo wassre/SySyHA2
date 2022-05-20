65e3680488032cb199201e2e68957c46fd548313
Miriam <61996077+glitsch@users.noreply.github.com>
2022-05-20 20:14:58 +0200

    Update views.py

--- part_2/todo/views.py
+++ part_2/todo/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import addItem
 
-# Create your views here.
-def todolist(request):
+def todolist(response):
     data = Item.objects.all()
-    return render(request, "todo/home.html", {'data':data})
+    return render(response, "todo/home.html", {'data':data})
 
-def impressum(request):
-    return render(request, "todo/impressum.html", {})
+def impressum(response):
+    return render(response, "todo/impressum.html", {})
+
+def new(response):
+    if response.method == "POST":
+        form = addItem(response.POST)
+        if form.is_valid():
+            t = form.cleaned_data["task"]
+            d = form.cleaned_data["deadline"]
+            p = form.cleaned_data["progress"]
+            i = Item(task=t, deadline=d, progress=p)
+            i.save()
+        return redirect('')
+    else:
+        form = addItem()
+    return render(response, 'todo/new.html', {"form":form})
+
+def edit(request, id):
+    if request.method == "GET":
+        item = Item.objects.get(id=id)
+        form = addItem(instance=item)
+        if form.is_valid():
+            item.task = form.cleaned_data["task"]
+            item.deadline = form.cleaned_data["deadline"]
+            item.progress = form.cleaned_data["progress"]
+            item.save()
+        return render(request, "todo/edit.html", {'form': form})
+    else:
+        item = Item.objects.get(id=id)
+        form = addItem(request.POST,instance=item)
+        if form.is_valid():
+            form.save()
+        return redirect('')
