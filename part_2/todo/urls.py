34afc8f9f5d527c69036ffe3e14816099015cac9
Miriam <61996077+glitsch@users.noreply.github.com>
2022-05-20 20:19:00 +0200

    new url paths

--- part_2/todo/urls.py
+++ part_2/todo/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.todolist, name=""),
     path("impressum/", views.impressum, name="impressum"),
+    path("new/", views.new, name="new"),
+    path("edit/<int:id>", views.edit, name="edit"),
 ]
