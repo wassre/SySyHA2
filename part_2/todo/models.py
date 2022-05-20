3ecc1a2aec3b0686289c55278388120b208517ff
wassre <alexander.tambunan@t-online.de>
2022-05-20 22:39:25 +0200

    added admin functionality

--- part_2/todo/models.py
+++ part_2/todo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 class Item(models.Model):
     task = models.CharField(max_length=160)
